a82e7054a4beb4ae0000f2495b900a53e4734e3a
pedroluanx28 <macielpedro794@gmail.com>
2025-07-02 21:27:24 -0300

    remove routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import { AppRoutes } from "./routing/AppRoutes.tsx";
+import { App } from "@/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<AppRoutes />
+		<App />
 	</StrictMode>
 );
